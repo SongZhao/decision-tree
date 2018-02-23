0efe46952396affb7c2830d713eb0982a8d8447f
Song Zhao <lanmovii@gmail.com>
2018-02-22 23:09:09 -0600

    .

--- dt-learn.py
+++ dt-learn.py
@@ -12,6 +12,17 @@ def takeInput():
 		ftrain = str(sys.argv[0])
 		ftest = str(sys.argv[1])
 		leaf_threshold = int(argv[2])
+		return ftrain, ftest, leaf_threshold
+
+
+
+def parseDate(file):
+	data, metadata = sa.loadaff(file)
+	feature_range = []
+	for name in metadata.names():
+		feature_range.append(metadata[name][1])
+	return data, metadata, feature_range
+
 
 
 
